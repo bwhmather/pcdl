19bc6f115a1e39b52c443b50f4c07411286313c1
Ben Mather <bwhmather@bwhmather.com>
2019-01-13 19:13:39 +0000

    Give output files better names

--- nand_gate.py
+++ nand_gate.py
@@ -1,12 +1,20 @@
 import pcdl
 
 
+filenames = [
+    'layer0_base_acrylic.svg',
+    'layer1_bottom_acrylic.svg',
+    'layer2_wells_acrylic.svg',
+    'layer3_membrane_silicone.svg',
+    'layer4_top_acrylic.svg',
+    'layer5_cover_acrylic.svg',
+]
+
+
 def main():
     layers = pcdl.load_gif('nand.gif')
-    filenames = []
 
-    for index, layer in enumerate(layers):
-        filename = f'layer{index}.svg'
+    for layer, filename in zip(layers, filenames):
         with open('build/' + filename, 'wb') as output:
             pcdl.render_layer(layer, output)
         filenames.append(filename)
