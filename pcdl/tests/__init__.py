ea32066088f82d09cff77a02f0c9dc8a3aa56e45
Ben Mather <bwhmather@bwhmather.com>
2019-01-20 14:17:28 +0000

    Remove unused bounding box class

--- pcdl/tests/__init__.py
+++ pcdl/tests/__init__.py
@@ -1,13 +1,11 @@
 import unittest
 
 from pcdl.tests import test_grid
-from pcdl.tests import test_bounding_box
 from pcdl.tests import test_svg
 
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
     loader.loadTestsFromModule(test_grid),
-    loader.loadTestsFromModule(test_bounding_box),
     loader.loadTestsFromModule(test_svg),
 ))
